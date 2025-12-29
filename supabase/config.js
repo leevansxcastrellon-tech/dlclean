df2d00a00d69251f3da9bd7f15c5a9c88228282f
leevansxcastrellon-tech <leevansxcastrellon@gmail.com>
2025-12-28 20:35:41 -0500

    anonkeys

--- supabase/config.js
+++ supabase/config.js
@@ -1,6 +1,6 @@
 const SUPABASE_CONFIG = {
     url: 'https://ehfebwvxvtadecmftlwt.supabase.co',
-    anonKey: ''
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoZmVid3Z4dnRhZGVjbWZ0bHd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY5NzEyNzAsImV4cCI6MjA4MjU0NzI3MH0.3dMU7foS502dQryZcVwEu-QWEAXLLqQXGf6LUz68dRE'
 };
 const TABLA_OPINIONES = 'opiniones';
 window.SUPABASE_CONFIG = SUPABASE_CONFIG;
